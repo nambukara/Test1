f726e6628b15d7ee588f44f02eede37031f95902
Thisen Nambukara <thisennambukara@gmail.com>
2024-11-27 20:19:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function facebook() {
+    alert("coming soon facebook");
+  }
+
+  function twitter() {
+    alert("coming soon twitter");
+  }  
+
+  function linkedin() {
+    alert("coming soon linkedin");
+  }
+  
+  function youtube() {
+    alert("coming soon youtube");
+  }  
+
+  function instagram() {
+    alert("coming soon instagram");
+  }  
